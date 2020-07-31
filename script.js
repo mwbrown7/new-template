f5e268e5a90b630dd182d573e200a13009c88aaa
mwbrown7 <villaboo3@gmail.com>
2020-07-31 16:44:58 -0400

    'initial'

---
+++ script.js
@@ -0,0 +1,70 @@
+const quoteContainer=document.getElementById("quote-container");
+const quoteText=document.getElementById("quote");
+const authorText=document.getElementById("author");
+const twitterBTn=document.getElementById("twitter");
+const newquoteBtn=document.getElementById("new-quote");
+const loader=document.getElementById("loader");
+
+
+
+function showLoadingSpinner(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+function removeLoadingSpinner(){
+
+    if(!loader.hidden){
+        quoteContainer.hidden=false;
+        loader.hidden=true;
+        
+    }
+}
+
+//Get quote from API
+async function getQuote(){
+    showLoadingSpinner();
+    const proxyUrl='https://vast-bayou-26306.herokuapp.com/'
+    const apiUrl='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response=await fetch( proxyUrl + apiUrl);
+        const data= await response.json();
+
+        // Author is blank add "Unknown"
+        if(data.quoteAuthor===''){
+            authorText.innerText='Unknown'
+        }else{
+            authorText.innerText=data.quoteAuthor;
+        }
+        // reduce font size for long quotes
+         if(data.quoteText.length>120){
+             quoteText.classList.add('long-quote')
+         }else{
+            quoteText.classList.remove('long-quote')
+         }
+         
+        quoteText.innerText=data.quoteText;
+        removeLoadingSpinner();
+    
+    }catch(error){
+        
+    getQuote();
+    }
+}
+
+
+
+//Tweet Quote
+function tweetQuote(){
+    const quote=quoteText.innerText;
+    const author=authorText.innerText;
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listeners for buttons
+newquoteBtn.addEventListener('click',getQuote);
+twitterBTn.addEventListener('click',tweetQuote)
+// On load
+getQuote();
+
